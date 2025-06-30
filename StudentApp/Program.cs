76b6ec08d64e44efd833d1b563bff7b2ebf89bd0
piyush.more <piyush.more@HP2356>
2025-06-29 23:26:51 -0700

    Add project files.

---
+++ StudentApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using StudentEncapApp;
+
+namespace StudentApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+
+            Console.WriteLine("Student Details:\n");
+
+            Console.Write("Enter Roll Number: ");
+            student1.RollNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Name (min 5 chars): ");
+            student1.Name = Console.ReadLine();
+
+            Console.Write("Enter CGPA (1 to 10): ");
+            student1.Cgpa = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(student1.ToString());
+        }
+    }
+}
---
+++ StudentApp/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEncapApp
+{
+    internal class Student
+    {
+        public int rollNo;
+        public string name;
+        public double cgpa;
+
+        public int RollNo
+        {
+            get { return rollNo; }
+            set { rollNo = value > 0 ? value : 0; }  //if value is greater than zero assign value to rollno else assign 0.
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value.Length >= 5 ? value : string.Empty; }
+        }
+
+        // cgpa 
+        public double Cgpa
+        {
+            get { return cgpa; }
+            set { cgpa = (value >= 1 && value <= 10) ? value : 0; }
+        }
+       //method for percentage
+        public double GetPercentage()
+        {
+            return Cgpa * 10;
+        }
+        //  override ToString method to display student details
+        public override string ToString()
+        {
+            return $"Roll No: {RollNo}, Name: {Name}, CGPA: {Cgpa}, Percentage: {GetPercentage()}%";
+        }
+    }
+}
